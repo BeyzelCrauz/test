b0630f0ee91802938d91795357cfd2523f1a0fde
Krauz Beyzel <sianara3@gmail.com>
2018-09-16 04:05:27 +0700

    first commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post (models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    name = models.ImageField(upload_to='static/', blank=True, verbose_name='22')
+    created_date = models.DateTimeField(
+        default=timezone.now)
+    def __str__(self):
+        return self.title
+
